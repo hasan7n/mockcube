bfc8244df46d03e77a2bf3eccdb07dbbe6390d60
hasan7n <hasankassim7@hotmail.com>
2022-08-06 15:56:09 +0200

    cubes

---
+++ prep-sep/project/check.py
@@ -0,0 +1,53 @@
+import os
+import yaml
+import argparse
+
+
+class SanityChecks:
+    def __init__(self, data_path, labels_path, params_file):
+        with open(params_file, "r") as f:
+            self.params = yaml.full_load(f)
+
+        self.data_path = data_path
+        self.labels_path = labels_path
+
+    def run(self):
+        ans = os.path.join(self.labels_path, "ans.csv")
+        nums = os.path.join(self.data_path, "nums.csv")
+        assert os.path.exists(ans), "file doesn't exist"
+        assert os.path.exists(nums), "file doesn't exist"
+        assert len(open(ans).read().strip().split("\n")) == len(
+            open(nums).read().strip().split("\n")
+        )
+        print("Prepared data sucessfully passed all tests")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--data_path",
+        "--data-path",
+        type=str,
+        required=True,
+        help="location of prepared data",
+    )
+    parser.add_argument(
+        "--labels_path",
+        "--labels-path",
+        type=str,
+        required=True,
+        help="location of prepared data",
+    )
+    parser.add_argument(
+        "--params_file",
+        "--params-file",
+        type=str,
+        required=True,
+        help="Configuration file for the data-preparation step",
+    )
+
+    args = parser.parse_args()
+    sanity_checker = SanityChecks(args.data_path, args.labels_path, args.params_file,)
+    sanity_checker.run()
+
